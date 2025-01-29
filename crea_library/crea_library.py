61852b37aed366b541f55ab4852f237571f5a29a
Alex Skitowski <alexskit@gmail.com>
2025-01-28 23:15:45 -0600

    Library for accessing CREA data

---
+++ crea_library/crea_library.py
@@ -0,0 +1,51 @@
+import json
+import scikit-learn as sklearn
+print(sklearn.__version__)
+from sklearn.metrics.pairwise import cosine_similarity
+
+with open('/Users/al33856/Downloads/selected_words (4).json') as f:
+    d = json.load(f)
+    print(d)
+
+class CREA_library:
+    def __init__(self, word_vectors):
+        self.word_vectors = word_vectors
+
+    def get_vector(self, word):
+        return self.word_vectors.get(word)
+    
+    def get_vectors(self, words):
+        selected_vecs = {}
+        for word in words:
+            vec = self.get_vector(word)
+            if vec is not None:
+                selected_vecs[word] = vec
+        return selected_vecs
+    
+    def select_cols(self, words, columns):
+        selected_vecs = {}
+        for word in words:
+            vec = self.get_vector(word)
+            if vec is not None:
+                selected_vecs[word] = [vec[col] for col in columns]
+        return selected_vecs
+    
+    def cosine_similarity(self, vec1, vec2):
+        vec1 = self.get_vector(vec1)
+        vec2 = self.get_vector(vec2)
+
+        if vec1 is None or vec2 is None:
+            return ValueError("Words not found")
+    
+        vec1 = np.array(vec1).reshape(1, -1)
+        vec2 = np.array(vec2).reshape(1, -1)
+        return cosine_similarity(vec1, vec2)[0][0]
+    
+
+all_vecs = CREA_library(d)
+print(all_vecs.get_vector('doctor'))
+print(all_vecs.get_vectors(['doctor', 'chocolate']))
+a = (all_vecs.select_cols(['doctor', 'peace'], [0,1]))
+# with open("data.json", "w") as f:
+#     json.dump(a, f)
+print(all_vecs.cosine_similarity('doctor', 'peace'))
