317a885594a6543db0ef976d715681a74a8ac5bc
Alex Skitowski <alexskit@gmail.com>
2025-02-17 12:06:02 -0600

    Update psychopy_collection_code .py

--- Data_Collection/Psychopy/psychopy_collection_code .py
+++ Data_Collection/Psychopy/psychopy_collection_code .py
@@ -1,4 +1,4 @@
-from psychopy import visual, event, core, logging
+from psychopy import visual, event, core
 import os 
 import csv
 import pandas as pd
@@ -9,9 +9,9 @@ c = 'category_define.csv'
 
 def load_word(file_name):
     df = pd.read_csv(file_name)
-    word = df.iloc[2,0]
-    ex1 = df.iloc[2,1]
-    ex2 = df.iloc[2,2]
+    word = df.iloc[13,0]
+    ex1 = df.iloc[13,1]
+    ex2 = df.iloc[13,2]
     return word, ex1, ex2
 
 def load_category(file_name):
@@ -39,7 +39,7 @@ stimuli['cat_def'] = visual.TextStim(win, text = cat_def_list[cat_index], pos = 
 stimuli['ex1_cat'] = visual.TextStim(win, text = ex1_def_list[cat_index], pos = (0.0, -0.20), italic = True, height = -0.01, wrapWidth = 2, anchorHoriz = 'center')
 stimuli['ex2_cat'] = visual.TextStim(win, text = ex2_def_list[cat_index], pos = (0.0, -0.25), italic = True, height = -0.01, wrapWidth = 2, anchorHoriz = 'center')
 
-#can't concat a list so asigning this as an object
+#can't concat a list so assigning it to a variable
 inst_word = word_text
 inst_cat = category_list[cat_index]
 
@@ -85,25 +85,23 @@ quit_keys = ["q", " "]
 
 #make empty results list to record what happened
 #this will be a list of dictionaries with the response to each category
-results_list = [{ 'word': inst_word, }]
+results_list = {inst_word: {}}
 
 #letting the user click the button
 #pressing the keys works as intended
-#clicking goes to a random category and doesn't save it properly
-#I want it to be able to work with both but even if you do only clicking it doesn't work
+
 while True:
     response = None
     while response is None:
         for i,btn in enumerate(buttons):
             if mouse.isPressedIn(btn):
                 response = str(i+1)
-#                results_list.append({
-#                    'word': inst_word,
-#                    'category': inst_cat,
-#                    'rating': response
-#                })
-                results_list[0][inst_cat] = response
+
+                results_list[inst_word][inst_cat] = float(response)
                 cat_index += 1
+                if cat_index >= len(category_list):
+                    print("You've completed all the ratings")
+                    break
                 inst_cat = category_list[cat_index]
                 core.wait(.2) #this seems to help the clitchiness. Not sure if theres a better soln but it works with both clicks and presses 
                 update_display()
@@ -112,8 +110,11 @@ while True:
         keys = event.getKeys()
         if any(key in valid_keys for key in keys):
             response = keys[0]
-            results_list[0][inst_cat] = response
+            results_list[inst_word][inst_cat] = float(response)
             cat_index += 1
+            if cat_index >= len(category_list):
+                    print("You've completed all the ratings")
+                    break
             inst_cat = category_list[cat_index]
             update_display()
         
@@ -125,11 +126,10 @@ while True:
     if cat_index >= len(category_list) or any(key in quit_keys for key in keys):  # auto stop if you reach the end 
         break #have to break out of the for loop and the while loop? 
         
-#save dict as json
+    #save dict as json
 with open("results.json", "w") as outfile:
     json.dump(results_list, outfile)
 print("results saved as json")
 
-    
 core.quit()
 
